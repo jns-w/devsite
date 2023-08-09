4ead3e565f9aa35d9f2b70401900e66b9b4d7608
jns-w <jns.w@icloud.com>
2023-08-09 09:23:42 +0800

    added: new components

---
+++ components/overlays/Overlays.tsx
@@ -0,0 +1,9 @@
+import styles from './overlays.module.scss'
+
+export function Overlays() {
+  return (
+    <div className={styles.wrapper}>
+
+    </div>
+  )
+}
---
+++ shared-components/modal/Modal.tsx
@@ -0,0 +1,12 @@
+import {motion} from 'framer-motion';
+import styles from './modal.module.scss';
+
+export default function Modal() {
+  return (
+    <motion.div
+      className={styles.wrapper}
+    >
+
+    </motion.div>
+  )
+}
